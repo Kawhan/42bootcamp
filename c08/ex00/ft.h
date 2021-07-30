c75f7c8e50f3c5e664b979f8a82168bb13454b30
klaurind <klaurind@student.42sp.org.br>
2021-07-30 08:49:33 +0000

    commit_08

---
+++ c08/ex00/ft.h
@@ -0,0 +1,11 @@
+#ifndef FT_H
+
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_swap(int *a, int *b);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
---
+++ c08/ex03/ft_point.h
@@ -0,0 +1,12 @@
+#ifndef FT_POINT_H
+# define FT_POINT_H
+
+typedef struct s_point	t_point;
+
+struct	s_point
+{
+	int	x;
+	int	y;
+};
+
+#endif
